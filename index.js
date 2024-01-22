685b9b091618665fea993bb0afc228b7d758ee8f
Otgonbyr <otgonbayar0503@gmail.om>
2024-01-22 23:36:14 +0800

    test

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+// type usersType = {
+//     name : string,
+//     age : number
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// const users : usersType[] = [
+//     {
+//         name: "Oggy",
+//         age: 20
+//     },
+//     {
+//         name: "angu",
+//         age: 12
+//     }
+// ]
+// console.log(users);
+////array
+// const sum = [1, 2, 3, 4]
+// console.log(sum);
+////primitive types
+// let decorations : boolean
+// decorations = true
+// console.log(decorations);
+////object types
+var id;
+id = {
+    name: "Oggy",
+    sum: 80899435,
+    val: true
+};
+console.log(id);
---
+++ index.ts
@@ -0,0 +1,43 @@
+// type usersType = {
+//     name : string,
+//     age : number
+// }
+
+// const users : usersType[] = [
+//     {
+//         name: "Oggy",
+//         age: 20
+//     },
+//     {
+//         name: "angu",
+//         age: 12
+//     }
+// ]
+// console.log(users);
+
+////array
+// const sum = [1, 2, 3, 4]
+// console.log(sum);
+
+////primitive types
+// let decorations : boolean
+// decorations = true
+// console.log(decorations);
+
+////object types
+// let id : {
+//     name: string,
+//     sum: number,
+//     val: boolean
+// }
+// id = {
+//     name: "Oggy",
+//     sum: 80899435,
+//     val: true
+// }
+// console.log(id);
+
+////Enum
+
+
+export {}
